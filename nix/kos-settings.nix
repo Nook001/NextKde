8cd5407f50df102a64a9f1ed20a2ccba7914c846
xiaoyintx <yinyue3260669043@outlook.com>
2026-09-06 18:13:51 +0800

    fix

--- nix/kos-settings.nix
+++ nix/kos-settings.nix
@@ -23,14 +23,19 @@ stdenv.mkDerivation {
     dontConfigure = true;
     dontWrapQtApps = true;
 
+    postPatch = ''
+        substituteInPlace apps/settings/CMakeLists.txt \
+            --replace-fail 'RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build"' 'RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}"'
+    '';
+
     installPhase = ''
         runHook preInstall
 
-        cmake -S "${src}/apps/settings" -B build -G Ninja \
+        cmake -S "${src}/apps/settings" -B "$TMPDIR/build" -G Ninja \
             -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX=$out
-        cmake --build build --parallel
-        cmake --install build
+        cmake --build "$TMPDIR/build" --parallel
+        cmake --install "$TMPDIR/build"
 
         mkdir -p $out/share/shared/qml
         cp -r $src/shared/qml/controls $out/share/shared/qml/controls
